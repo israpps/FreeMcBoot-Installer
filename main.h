c18d7e2bb35bf71736ced14e004a33010b1d98fa
israpps <isr_apps_help@hotmail.com>
2021-04-29 16:11:12 -0300

    Update main.h

--- main.h
+++ main.h
@@ -6,7 +6,7 @@
 	#define DEBUG_PRINTF(args...)
 #endif
 
-#define FMCB_INSTALLER_VERSION	"1.000-Standard"
+#define FMCB_INSTALLER_VERSION	"1.000"
 
 /* The number of files and folders to crosslink (For multi-regional and cross-model installations). */
 #define NUM_CROSSLINKED_FILES	8
