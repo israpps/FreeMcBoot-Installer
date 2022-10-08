bb8f037756c476229448ef4055464a1d04e0dd9b
Matías Israelson <57065102+israpps@users.noreply.github.com>
2022-10-08 17:54:17 -0300

    clang'format `1.966`

--- 1966/font.h
+++ 1966/font.h
@@ -1,17 +1,16 @@
-#define FNT_ATLAS_WIDTH		128
-#define FNT_ATLAS_HEIGHT	128
-#define FNT_MAX_ATLASES		4
-#define FNT_CHAR_WIDTH		16
-#define FNT_CHAR_HEIGHT		16
-#define FNT_TAB_STOPS		4
+#define FNT_ATLAS_WIDTH  128
+#define FNT_ATLAS_HEIGHT 128
+#define FNT_MAX_ATLASES  4
+#define FNT_CHAR_WIDTH   16
+#define FNT_CHAR_HEIGHT  16
+#define FNT_TAB_STOPS    4
 
 int FontInit(struct UIDrawGlobal *gsGlobal, const char *FontFile);
 int FontInitWithBuffer(struct UIDrawGlobal *gsGlobal, void *buffer, unsigned int size);
 int AddSubFont(struct UIDrawGlobal *gsGlobal, const char *FontFile);
 int AddSubFontWithBuffer(struct UIDrawGlobal *gsGlobal, void *buffer, unsigned int size);
 void FontDeinit(void);
-int FontReset(struct UIDrawGlobal *gsGlobal);	//Performs a partial re-initialization of the Font library and re-allocates VRAM. Used when VRAM has been cleared.
+int FontReset(struct UIDrawGlobal *gsGlobal); // Performs a partial re-initialization of the Font library and re-allocates VRAM. Used when VRAM has been cleared.
 void FontPrintfWithFeedback(struct UIDrawGlobal *gsGlobal, short int x, short int y, short int z, float scale, GS_RGBAQ colour, const char *string, short int *xRel, short int *yRel);
 void FontPrintf(struct UIDrawGlobal *gsGlobal, short int x, short int y, short int z, float scale, GS_RGBAQ colour, const char *string);
 int FontGetGlyphWidth(struct UIDrawGlobal *gsGlobal, wint_t character);
-
--- 1966/system.h
+++ 1966/system.h
@@ -1,53 +1,55 @@
-enum BootDeviceIDs{
-	BOOT_DEVICE_UNKNOWN = -1,
-	BOOT_DEVICE_MASS,
+enum BootDeviceIDs {
+    BOOT_DEVICE_UNKNOWN = -1,
+    BOOT_DEVICE_MASS,
 
-	BOOT_DEVICE_COUNT,
+    BOOT_DEVICE_COUNT,
 };
 
-enum CONSOLE_REGION{
-	CONSOLE_REGION_JAPAN	= 0,
-	CONSOLE_REGION_USA,	//USA and Asia
-	CONSOLE_REGION_EUROPE,
-	CONSOLE_REGION_CHINA,
+enum CONSOLE_REGION {
+    CONSOLE_REGION_JAPAN = 0,
+    CONSOLE_REGION_USA, // USA and Asia
+    CONSOLE_REGION_EUROPE,
+    CONSOLE_REGION_CHINA,
 
-	CONSOLE_REGION_COUNT
+    CONSOLE_REGION_COUNT
 };
 
-#define FILE_IS_KELF		0x01
+#define FILE_IS_KELF 0x01
 
-enum EXTENDED_ERROR_CODES{
-	EEXTMGSIGNERR	 = 0x1000,
-	EEXTCRSLNKFAIL,
-	EEXTCACHEINITERR,	//MCTools cache initialization error.
-	EEXTUNSUPPUNINST	//Unsupported uninstallation file.
+enum EXTENDED_ERROR_CODES {
+    EEXTMGSIGNERR = 0x1000,
+    EEXTCRSLNKFAIL,
+    EEXTCACHEINITERR, // MCTools cache initialization error.
+    EEXTUNSUPPUNINST  // Unsupported uninstallation file.
 };
 
-//Bits for indicating the side the error is on.
-#define ERROR_SIDE_DST	0x4000
-#define ERROR_SIDE_SRC	0x8000
+// Bits for indicating the side the error is on.
+#define ERROR_SIDE_DST 0x4000
+#define ERROR_SIDE_SRC 0x8000
 
-struct InstallationFile{
-	const char *SrcRelPath;
-	char *DestRelPath;
-	unsigned int flags;
+struct InstallationFile
+{
+    const char *SrcRelPath;
+    char *DestRelPath;
+    unsigned int flags;
 };
 
-#define INSTALL_MODE_FLAG_SKIP_CNF	0x01
-#define INSTALL_MODE_FLAG_MULTI_INST	0x02
-#define INSTALL_MODE_FLAG_CROSS_REG	0x04	//Note: This cannot be set with INSTALL_MODE_FLAG_MULTI_INST together. It cannot be set with INSTALL_MODE_FLAG_CROSS_MODEL either.
-#define INSTALL_MODE_FLAG_CROSS_MODEL	0x08	//Note: This cannot be set with INSTALL_MODE_FLAG_MULTI_INST together. It cannot be set with INSTALL_MODE_FLAG_CROSS_REG either.
-#define INSTALL_MODE_FLAG_CROSS_PSX	0x10
-#define INSTALL_MODE_FLAG_SKIP_CLEANUP	0x20
+#define INSTALL_MODE_FLAG_SKIP_CNF     0x01
+#define INSTALL_MODE_FLAG_MULTI_INST   0x02
+#define INSTALL_MODE_FLAG_CROSS_REG    0x04 // Note: This cannot be set with INSTALL_MODE_FLAG_MULTI_INST together. It cannot be set with INSTALL_MODE_FLAG_CROSS_MODEL either.
+#define INSTALL_MODE_FLAG_CROSS_MODEL  0x08 // Note: This cannot be set with INSTALL_MODE_FLAG_MULTI_INST together. It cannot be set with INSTALL_MODE_FLAG_CROSS_REG either.
+#define INSTALL_MODE_FLAG_CROSS_PSX    0x10
+#define INSTALL_MODE_FLAG_SKIP_CLEANUP 0x20
 
-struct FileCopyTarget{
-	char *source, *target;
-	unsigned int size, mode, flags;
-	void *buffer;
+struct FileCopyTarget
+{
+    char *source, *target;
+    unsigned int size, mode, flags;
+    void *buffer;
 };
 
-#define UNINST_FILE_VERSION	0x110
-#define UNINST_FILE_VERSION_100	0x100	//Old uninstallation file format.
+#define UNINST_FILE_VERSION     0x110
+#define UNINST_FILE_VERSION_100 0x100 // Old uninstallation file format.
 
 /*	The structure of the v1.10 multi-installation uninstallation data file is as follows:
  *
@@ -58,37 +60,39 @@ struct FileCopyTarget{
  *		0x0A+	- The path of the cross-linked files, 66-bytes for each record (see the name field of the FileAlias structure within MCTOOLS/mctools.h).
  */
 
-struct UninstallationDataFileHeader{
-	char signature[2];		/* "FB" */
-	unsigned short int version;
+struct UninstallationDataFileHeader
+{
+    char signature[2]; /* "FB" */
+    unsigned short int version;
 };
 
-enum PlayStation2SystemType{
-	PS2_SYSTEM_TYPE_PS2	= 0,
-	PS2_SYSTEM_TYPE_PSX,	//DVR unit
-	PS2_SYSTEM_TYPE_DEX	//DebugStation unit
+enum PlayStation2SystemType {
+    PS2_SYSTEM_TYPE_PS2 = 0,
+    PS2_SYSTEM_TYPE_PSX, // DVR unit
+    PS2_SYSTEM_TYPE_DEX  // DebugStation unit
 };
 
-enum WorkerThreadCommandCodes{
-	WORKER_THREAD_CMD_NONE	= 0,
-	WORKER_THREAD_CMD_DUMP_MC,
-	WORKER_THREAD_CMD_RESTORE_MC,
-	WORKER_THREAD_CMD_STOP	//Stop ongoing operations.
+enum WorkerThreadCommandCodes {
+    WORKER_THREAD_CMD_NONE = 0,
+    WORKER_THREAD_CMD_DUMP_MC,
+    WORKER_THREAD_CMD_RESTORE_MC,
+    WORKER_THREAD_CMD_STOP // Stop ongoing operations.
 };
 
-enum WorkerThreadResultCodes{
-	WORKER_THREAD_RES_ERR	= -1,	//Error codes are all <0.
-	WORKER_THREAD_RES_OK	= 0,
-	WORKER_THREAD_RES_CMD_OK,	//Command acknowledged and understood.
-	WORKER_THREAD_RES_BSY,
-	WORKER_THREAD_RES_ABRT
+enum WorkerThreadResultCodes {
+    WORKER_THREAD_RES_ERR = -1, // Error codes are all <0.
+    WORKER_THREAD_RES_OK = 0,
+    WORKER_THREAD_RES_CMD_OK, // Command acknowledged and understood.
+    WORKER_THREAD_RES_BSY,
+    WORKER_THREAD_RES_ABRT
 };
 
-struct WorkerThreadMcMaintParams{	//MC Maintanence parameters. For MC dumping and restoration.
-	int port, slot;
-	FILE *file;
-	unsigned int ClusterSize;	//Number of pages cluster. Used only in a read operation.
-	const struct MCTools_McSpecData *McSpecData;
+struct WorkerThreadMcMaintParams
+{ // MC Maintanence parameters. For MC dumping and restoration.
+    int port, slot;
+    FILE *file;
+    unsigned int ClusterSize; // Number of pages cluster. Used only in a read operation.
+    const struct MCTools_McSpecData *McSpecData;
 };
 
 /* Function prototypes */
@@ -134,4 +138,3 @@ int HDDCheckHasSpace(unsigned int PartSize);
 int HDDCheckStatus(void);
 
 void poweroffCallback(void *arg);
-
