e7d9d7944327b2747bc314a5f71785ae10ef8820
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-01-29 19:16:26 -0300

    print argumments on init via serial port

--- 1965/main.c
+++ 1965/main.c
@@ -58,6 +58,9 @@ int main(int argc, char *argv[])
     ee_sema_t sema;
     sio_init(38400, 0, 0, 0, 0);
 
+    for (result=0; result<argc;result++)
+        sio_printf("\targv[%d] = %s\n", argc, argv[result]);
+
     // chdir("mass:/FMCBInstaller/");
     if ((BootDevice = GetBootDeviceID()) == BOOT_DEVICE_UNKNOWN)
     {
@@ -76,6 +79,7 @@ int main(int argc, char *argv[])
     EnableIntc(kINTC_VBLANK_START);
 
     if (InitializeUI(0) != 0) {
+        sio_printf("InitializeUI(0) failed \n");
         SifExitRpc();
         Exit(-1);
     }
