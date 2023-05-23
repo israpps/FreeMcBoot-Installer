d1e9e9d0bd523042157da7326136a60582fe6fc1
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-05-23 01:37:20 -0300

    reduce code duplication by unifying all FMCB packages under same installer (#23)
    [skip actions]

--- 1965/pad.h
+++
@@ -1,7 +0,0 @@
-void PadInitPads(void);
-void PadDeinitPads(void);
-
-int ReadPadStatus_raw(int port, int slot);
-int ReadCombinedPadStatus_raw(void);
-int ReadPadStatus(int port, int slot);
-int ReadCombinedPadStatus(void);
---
+++ installer_res/pack.sh
@@ -0,0 +1,16 @@
+
+PKG_DATE=$(date '+[%Y-%m-%d]')
+cp ../Changelog.md __base/Changelog.md
+for subdir in 1966 1965 1953
+do
+    echo packing v$subdir into ../FMCB-$subdir.7z
+    NEWDIR="FMCBinst-$subdir-$PKG_DATE"
+    cp -r __base/ $NEWDIR/
+    cp -r $subdir/INSTALL/ $NEWDIR/INSTALL/
+    echo $SHA8>$NEWDIR/lang/commit.txt
+    echo "title=FreeMcBoot v$subdir $PKG_DATE" >$NEWDIR/title.cfg
+    echo "boot=FMCBInstaller.elf">>$NEWDIR/title.cfg
+    cp FMCBInstaller.elf $NEWDIR/
+    cp FMCBInstaller_EXFAT.elf $NEWDIR/
+    7z a -t7z -r ../FMCB-$subdir.7z $NEWDIR/*
+done
