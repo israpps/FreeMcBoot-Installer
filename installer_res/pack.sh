67298d8376bef36ed5bd22a90f4cd4fb61afc9b5
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-12-19 16:16:47 -0300

    Bundle files to make FreeMcBoot support EXFAT USB (#28)
    Along with instructions

--- installer_res/pack.sh
+++ installer_res/pack.sh
@@ -1,5 +1,7 @@
 
 PKG_DATE=$(date '+[%Y-%m-%d]')
+wget https://github.com/israpps/BDMAssault/releases/download/latest/BDMAssault.7z -O BDMAssault.7z
+7z x BDMAssault.7z READY_TO_USE/FreeMcBoot/
 cp ../Changelog.md __base/Changelog.md
 for subdir in 1966 1965 1964 1963 1953
 do
@@ -12,5 +14,8 @@ do
     echo "boot=FMCBInstaller.elf">>$NEWDIR/title.cfg
     cp FMCBInstaller.elf $NEWDIR/
     cp FMCBInstaller_EXFAT.elf $NEWDIR/
+    mkdir -p $NEWDIR/FMCB_EXFAT/
+    cp -r READY_TO_USE/FreeMcBoot/SYS-CONF/ $NEWDIR/FMCB_EXFAT/
+    cp EXFAT_INSTALL_INSTRUCTIONS.TXT $NEWDIR/FMCB_EXFAT/INSTRUCTIONS.TXT
     7z a -t7z -r ../FMCB-$subdir.7z $NEWDIR/*
 done
